73dd1cecdf7c7e8baa77ebd259d31aabc1829b59
donia ziyad <doniaziyad@hotmail.com>
2023-02-04 12:55:59 +0200

    avg.php and sum.php added

---
+++ avg.php
@@ -0,0 +1,8 @@
+<?php
+
+$nums = [10, 20, 39];
+$sum =0;
+foreach ($nums as $item) {
+    $sum += $item;
+}
+echo $avg=$sum/5;
---
+++ sum.php
@@ -0,0 +1,9 @@
+<?php
+$nums=[10,20,39];
+$sum=0;
+foreach ($nums as $item) {
+    $sum+=$item;
+}
+echo $sum;
+
+
